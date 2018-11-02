d5a7ddbef69086ad54f3bca641d600f919e3367b
stcoyle <22sc01@cf.k12.mn.us>
2018-11-02 07:43:39 -0500

    try

--- LongTerm 2018/src/CFHS/LegoRobots/main/Sensor.java
+++ LongTerm 2018/src/CFHS/LegoRobots/main/Sensor.java
@@ -1,5 +1,19 @@
 package CFHS.LegoRobots.main;
 
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.sensor.*;
+
 interface Sensor{
 	public double getValue();
+}
+private class Port{
+	public Port(int port)
+}
+class TouchSensor implements Sensor{
+	public TouchSensor(int port) {
+		Brick brick = BrickFinder.getDefault();
+	    lejos.hardware.port.Port  = brick.getPort("S4");
+		EV3TouchSensor = new EV3TouchSensor();
+	}
 }
