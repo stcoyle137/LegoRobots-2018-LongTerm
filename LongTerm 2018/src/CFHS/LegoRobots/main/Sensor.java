18f62cea5b900af99093cccb661dadf64ad72ebc
izzy-el <42899967+izzy-el@users.noreply.github.com>
2018-11-02 07:40:30 -0500

    Update on The ColorSensor and GyroSensor.

--- LongTerm 2018/src/CFHS/LegoRobots/main/Sensor.java
+++ LongTerm 2018/src/CFHS/LegoRobots/main/Sensor.java
@@ -1,5 +1,44 @@
 package CFHS.LegoRobots.main;
 
+import java.lang.reflect.Array;
+
+import lejos.hardware.sensor.*;
+import lejos.robotics.SampleProvider;
+
+
 interface Sensor{
 	public double getValue();
+}
+
+class TouchSensor implements Sensor{
+	public TouchSensor (int port) {
+		
+	}
+}
+
+class ColorSensor implements Sensor {
+	private EV3ColorSensor colorSensor;
+	public ColorSensor (int port) {
+		colorSensor = new EV3ColorSensor(null);
+	}
+	public double getValue() {
+		return colorSensor.getColorID();
+	}
+}	
+
+class GyroSensor implements Sensor {
+	private EV3GyroSensor gyroSensor;
+	private float[] sample;
+	private SampleProvider gyroReader;
+	
+	public GyroSensor (int port) {
+		gyroSensor = new EV3GyroSensor(null);
+		gyroSensor.reset();
+		gyroReader = gyroSensor.getAngleMode();
+		sample = new float[gyroReader.sampleSize()];
+	}
+	public double getValue() {
+		gyroSensor.fetchSample(sample, 0);
+		return sample[0];
+	}
 }
