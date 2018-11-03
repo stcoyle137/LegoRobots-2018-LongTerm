b86e14d932a425138f130573f62b55bf2bcd987e
stcoyle <22sc01@cf.k12.mn.us>
2018-11-02 22:27:14 -0500

    This code has a lot of errors. will fix

--- LongTerm 2018/src/CFHS/LegoRobots/main/Sensor.java
+++ LongTerm 2018/src/CFHS/LegoRobots/main/Sensor.java
@@ -8,12 +8,44 @@ interface Sensor{
 	public double getValue();
 }
 private class Port{
-	public Port(int port);
+	private SensorPort p;
+	public Port(int port) {
+		switch (port) {
+		case 1:
+			p 
+		}
+	};
 } 
 class TouchSensor implements Sensor{
 	public TouchSensor(int port) {
 		Brick brick = BrickFinder.getDefault();
 	    lejos.hardware.port.Port  = brick.getPort("S4");
 		EV3TouchSensor = new EV3TouchSensor();
+	}
+}
+class ColorSensor implements Sensor {
+	private EV3ColorSensor colorSensor;
+	public ColorSensor (int port) {
+		colorSensor = new EV3ColorSensor(null);
+	}
+	public double getValue() {
+		return colorSensor.getColorID();
+	}
+}	
+
+class GyroSensor implements Sensor {
+	private EV3GyroSensor gyroSensor;
+	private float[] sample;
+	private SampleProvider gyroReader;
+	
+	public GyroSensor (int port) {
+		gyroSensor = new EV3GyroSensor(null);
+		gyroSensor.reset();
+		gyroReader = gyroSensor.getAngleMode();
+		sample = new float[gyroReader.sampleSize()];
+	}
+	public double getValue() {
+		gyroSensor.fetchSample(sample, 0);
+		return sample[0];
 	}
 }
