67dc66f58774bcd7e87aecd8c31eb18625f2c05d
stcoyle <22sc01@cf.k12.mn.us>
2018-11-09 18:49:22 -0600

    got rid of a long unneed comment

--- LongTerm 2018/src/CFHS/LegoRobots/main/Sensor.java
+++ LongTerm 2018/src/CFHS/LegoRobots/main/Sensor.java
@@ -12,40 +12,6 @@ interface Sensor{
 	public String getPort();
 }
 
-
-//class TouchSensor implements Sensor{
-//	public TouchSensor (int port) {
-//		
-//	}
-//}
-//
-//class ColorSensor implements Sensor {
-//	private EV3ColorSensor colorSensor;
-//	public ColorSensor (int port) {
-//		colorSensor = new EV3ColorSensor(null);
-//	}
-//	public double getValue() {
-//		return colorSensor.getColorID();
-//	}
-//}	
-//
-//class GyroSensor implements Sensor {
-//	private EV3GyroSensor gyroSensor;
-//	private float[] sample;
-//	private SampleProvider gyroReader;
-//	
-//	public GyroSensor (int port) {
-//		gyroSensor = new EV3GyroSensor(null);
-//		gyroSensor.reset();
-//		gyroReader = gyroSensor.getAngleMode();
-//		sample = new float[gyroReader.sampleSize()];
-//	}
-//	public double getValue() {
-//		gyroSensor.fetchSample(sample, 0);
-//		return sample[0];
-//	}
-//}
-
 class PortForSensors{
 	private lejos.hardware.port.Port port;
 	private String whichPort;
