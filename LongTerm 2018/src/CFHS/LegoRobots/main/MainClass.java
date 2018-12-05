23254bdec4cb7c14f7414b004b97c95819e3dedf
stcoyle <22sc01@cf.k12.mn.us>
2018-12-04 19:00:51 -0600

    Stuff changed

--- LongTerm 2018/src/CFHS/LegoRobots/main/MainClass.java
+++ LongTerm 2018/src/CFHS/LegoRobots/main/MainClass.java
@@ -4,11 +4,11 @@ import lejos.utility.Delay;
 
 public class MainClass {
 
-	public static Robot r1 = new Robot("eete","llle");
+	public static Robot r1 = new Robot("ecte","llle");
 	public static boolean setUp = false;
 	
 	public static void main(String[] args) {
-		rampWithGap();
+		pyramid();
 	}
 	
 	public static void oneBoard() {
@@ -37,9 +37,26 @@ public class MainClass {
 			if(r1.getValueForSensor(3) == 1) {
 				r1.runMotorDegrees('a', 45*5, -.5);
 				Delay.msDelay(100);
-				r1.runMotorDegrees('a',135*5, -.5);
+				r1.runMotorDegrees('a',135*5, -0.5);
 				r1.stopSyncMotor();
-				r1.runMotorDegrees('a', 180*5, -.5);
+				r1.runMotorDegrees('a', 180*5, -0.5);
+				break;
+			}
+		}
+		
+	}
+	
+	public static void pyramid() {
+		if(!setUp) {
+			r1.setUpMotorSystem('b', 'c');
+			setUp = true;
+		}
+		r1.runSyncMotor(.4, .4);
+		while(true) {
+			if(r1.getValueForSensor(3) == 1) {
+				r1.runMotorDegrees('a', 900*5, -0.5);
+				r1.stopSyncMotor();
+				r1.runMotorDegrees('a', 180*5, -0.5);
 				break;
 			}
 		}
