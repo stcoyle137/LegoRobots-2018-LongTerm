7de75834de041132c027cf679bdd28bfedcf6196
stcoyle <22sc01@cf.k12.mn.us>
2018-12-07 07:54:07 -0600

    Here you go Izzy

--- LongTerm 2018/src/CFHS/LegoRobots/main/MainClass.java
+++ LongTerm 2018/src/CFHS/LegoRobots/main/MainClass.java
@@ -1,14 +1,23 @@
 package CFHS.LegoRobots.main;
 
 import lejos.utility.Delay;
+import lejos.robotics.Color;
 
 public class MainClass {
 
 	public static Robot r1 = new Robot("ecte","llle");
 	public static boolean setUp = false;
-	
+
 	public static void main(String[] args) {
-		pyramid();
+		r1.setUpMotorSystem('b', 'c');
+		while(r1.getValueForSensor(2) != Color.BLUE) {
+			greenTapeFollow();
+		}
+		r1.runSyncMotor(.5, -.5);
+		Delay.msDelay(1000);
+		while(true) {
+			blueTapeFollow();
+		}
 	}
 	
 	public static void oneBoard() {
@@ -57,9 +66,28 @@ public class MainClass {
 				r1.runMotorDegrees('a', 900*5, -0.5);
 				r1.stopSyncMotor();
 				r1.runMotorDegrees('a', 180*5, -0.5);
-				break;
+					break;
+				}
 			}
 		}
-		
+
+	public static void blueTapeFollow() {
+		r1.setUpMotorSystem('b', 'c');
+		if(r1.getValueForSensor(2) == Color.BLUE) {
+			r1.runSyncMotor(.4, .2);
+		}
+		else {
+			r1.runSyncMotor(.2, .4);
+		}
+	}
+	
+	public static void greenTapeFollow() {
+		r1.setUpMotorSystem('b', 'c');
+		if(r1.getValueForSensor(2) == Color.GREEN) {
+			r1.runSyncMotor(.4, .2);
+		}
+		else {
+			r1.runSyncMotor(.2, .4);
+		}
 	}
 }
